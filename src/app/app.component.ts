a5381ed177c7f8fa4705d657b0376c44b3980868
Venky Venkataraman <venky76v@gmail.com>
2019-08-15 12:05:05 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable, of, combineLatest } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { State, states } from './_models/state';
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-filtering-rxjs';
+  state$: Observable<State[]>;
+  filteredState$: Observable<State[]>;
+  filter: FormControl;
+  filter$: Observable<string>;
+
+  constructor(private http: HttpClient) {
+    this.state$ = of(states);
+    console.log(this.state$);
+    this.filter = new FormControl('');
+    this.filter$ = this.filter.valueChanges.pipe(startWith(''));
+    combineLatest(this.state$, this.filter$).subscribe(data => console.log(data));
+    this.filteredState$ = combineLatest(this.state$, this.filter$).pipe(
+      map(([states, filterString]) => states.filter(state => state.name.indexOf(filterString) !== -1)));
+  }
 }
